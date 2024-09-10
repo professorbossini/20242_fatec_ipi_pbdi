a4fa8e9d98ca6fbb9c6bc6bf2ee8a2a1009b2090
Rodrigo Bossini <professorbossini@gmail.com>
2024-09-10 09:36:13 -0300

    ap07: ends material and proposes exercise
    - students supposed to resolve both 01(git calculator) and 07(python login)
    for next class

--- login_python/login.py
+++ login_python/login.py
@@ -31,10 +31,31 @@ def existe(usuario):
     #    return True
     # return False
 
+def menu():
+  texto = "0-Fechar\n1-Login\n2-Logoff"
+  usuario = None
+  op = int(input(texto))
+  while op != 0:
+    if op == 1:
+      login = input('Digite seu login\n')
+      senha = input('Digite sua senha\n')
+      usuario = Usuario(login, senha)
+      print('Usuário OK!' if existe(usuario) else 'Usuário NOK!')
+    elif op == 2:
+      usuario = None
+      print('Logoff realizado com sucesso')
+    elif op == 3:
+      #permitir o cadastro de um novo usuário
+      pass
+    op = int(input(texto))
+  else:
+     print('Até mais')
+      
 def main():
-   login = 'admin'
-   senha = 'admin'
-   usuario = Usuario(login, senha)
-   print("Existe" if existe(usuario) else "Não existe")
+  menu()
+  #  login = 'admina'
+  #  senha = 'admin'
+  #  usuario = Usuario(login, senha)
+  #  print("Existe" if existe(usuario) else "Não existe")
 
 main()
