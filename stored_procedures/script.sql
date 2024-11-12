b86c80b5f8c18e19b02d126e341a712cd8e60891
Rodrigo Bossini <professorbossini@gmail.com>
2024-11-12 11:17:42 -0300

    ap11(sp): encerra a apostila
    - a minimização de notas ficou como desafio

--- stored_procedures/script.sql
+++ stored_procedures/script.sql
@@ -1,20 +1,211 @@
--- Active: 1724757618200@@127.0.0.1@5432@20242_fatec_ipi_pbdi_rodrigo@public
+-- Active: 1731007942713@@127.0.0.1@5432@20242_fatec_ipi_pbdi_rodrigo@public
 
-DROP TABLE IF EXISTS tb_cliente;
-CREATE TABLE tb_cliente(
-  cod_cliente SERIAL PRIMARY KEY,
-  nome VARCHAR(200) NOT NULL
-);
+--14: escrever um proc que minimiza o número de notas a serem utilizadas para representar o troco
 
-DROP TABLE IF EXISTS tb_pedido;
-CREATE TABLE IF NOT EXISTS tb_pedido(
-  cod_pedido SERIAL PRIMARY KEY,
-  data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  data_modificacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  status VARCHAR DEFAULT 'aberto',
-  cod_cliente INT NOT NULL,
-  CONSTRAINT fk_cliente FOREIGN KEY (cod_cliente) REFERENCES tb_cliente(cod_cliente)
-);
+--calcular o valor de troco
+
+DO $$
+DECLARE
+  v_troco INT;
+  v_valor_total INT;
+  v_valor_a_pagar INT := 100;
+BEGIN
+  CALL sp_calcular_valor_de_um_pedido(1, v_valor_total);
+  CALL sp_calcular_troco(v_troco, v_valor_a_pagar, v_valor_total);
+  RAISE NOTICE 'A conta foi de R$% e você pagou R$%. Portanto, seu troco é R$%.', v_valor_total, v_valor_a_pagar, v_troco;
+END;
+$$;
+-- CREATE OR REPLACE PROCEDURE sp_calcular_troco(
+--   OUT p_troco INT,
+--   IN p_valor_a_pagar INT,
+--   IN p_valor_total INT
+-- ) LANGUAGE plpgsql AS $$
+-- BEGIN
+--   p_troco := p_valor_a_pagar - p_valor_total;
+-- END;
+-- $$;
+
+
+--calcular valor total de um pedido
+-- SELECT * FROM tb_pedido;
+
+-- DO $$
+-- BEGIN
+--   CALL sp_fechar_pedido(200, 1);
+-- END;
+-- $$;
+
+
+-- CREATE OR REPLACE PROCEDURE sp_fechar_pedido(
+--   IN p_valor_a_pagar INT,
+--   IN p_cod_pedido INT
+-- ) LANGUAGE plpgsql AS $$
+-- DECLARE
+--   v_valor_total INT;
+-- BEGIN
+--   --vamos verificar se o valor a pagar é suficiente
+--   CALL sp_calcular_valor_de_um_pedido(p_cod_pedido, v_valor_total);
+--   IF p_valor_a_pagar < v_valor_total THEN
+--     RAISE NOTICE 
+--       'R$% insuficiente para pagar a conta de R$%', 
+--       p_valor_a_pagar, v_valor_total;
+--   ELSE
+--     UPDATE tb_pedido SET
+--       data_modificacao = CURRENT_TIMESTAMP,
+--       status = 'fechado'
+--       WHERE cod_pedido = $2;
+--   END IF;
+-- END;
+-- $$;
+
+
+-- DO $$
+-- DECLARE
+--   v_valor_total INT;
+-- BEGIN
+--   CALL sp_calcular_valor_de_um_pedido(1, v_valor_total);
+--   RAISE NOTICE 'Total do pedido %: R$%.', 1, v_valor_total; 
+-- END;
+-- $$;
+
+-- CREATE OR REPLACE PROCEDURE sp_calcular_valor_de_um_pedido(
+--   IN p_cod_pedido INT,
+--   OUT p_valor_total INT
+-- ) LANGUAGE plpgsql AS $$
+-- BEGIN
+--   SELECT SUM(i.valor) FROM
+--     tb_pedido p
+--   INNER JOIN tb_item_pedido ip ON
+--     p.cod_pedido = ip.cod_pedido
+--   INNER JOIN tb_item i ON
+--     ip.cod_item = i.cod_item
+--   WHERE p.cod_pedido = p_cod_pedido
+--   INTO p_valor_total;
+-- END;
+-- $$;
+
+
+-- SELECT * FROM tb_pedido;
+-- SELECT * FROM tb_item;
+
+-- SELECT * FROM tb_item_pedido;
+-- CALL sp_adicionar_item_a_pedido(1, 1);
+
+--adicionar um item a um pedido
+-- CREATE OR REPLACE PROCEDURE sp_adicionar_item_a_pedido(
+--   IN p_cod_item INT,
+--   IN p_cod_pedido INT
+-- ) LANGUAGE plpgsql AS $$
+-- BEGIN
+--   --insere novo item
+--   INSERT INTO tb_item_pedido (cod_item, cod_pedido) VALUES($1, $2);
+--   --atualiza data de modificação do pedido
+--   UPDATE tb_pedido p 
+--     SET data_modificacao = CURRENT_TIMESTAMP
+--       WHERE p.cod_pedido = p_cod_pedido;  
+-- END;
+-- $$;
+
+
+-- DO $$
+-- DECLARE
+--   v_cod_pedido INT;
+--   v_cod_cliente INT;
+-- BEGIN
+--   SELECT c.cod_cliente FROM tb_cliente c WHERE nome  LIKE 'João' INTO v_cod_cliente;
+--   CALL sp_criar_pedido(v_cod_pedido, v_cod_cliente);
+--   RAISE NOTICE 'Código do pedido recém criado: %', v_cod_pedido;
+-- END;
+-- $$;
+
+
+
+--cadastro de novos pedidos
+-- CREATE OR REPLACE PROCEDURE sp_criar_pedido(
+--   OUT p_cod_pedido INT,
+--   IN p_cod_cliente INT
+-- ) LANGUAGE plpgsql
+-- AS $$
+-- BEGIN
+--   INSERT INTO tb_pedido(cod_cliente) VALUES (p_cod_cliente);
+--   SELECT LASTVAL() INTO p_cod_pedido;
+-- END;
+-- $$;
+
+--cadastro de cliente
+-- CALL sp_cadastrar_cliente('João');
+-- CALL sp_cadastrar_cliente('Maria');
+-- SELECT * FROM tb_cliente;
+--vamos usar um parâmetro com valor padrão
+-- CREATE OR REPLACE PROCEDURE sp_cadastrar_cliente(
+--   IN p_nome VARCHAR(200), IN p_cod_cliente INT DEFAULT NULL
+-- ) LANGUAGE plpgsql AS $$
+-- BEGIN
+--   IF p_cod_cliente IS NULL THEN
+--     INSERT INTO tb_cliente(nome) VALUES(p_nome);
+--   ELSE
+--     INSERT INTO tb_cliente(cod_cliente, nome) 
+--       VALUES(p_cod_cliente, p_nome);
+--   END IF;
+-- END;
+-- $$;
+
+
+-- DROP TABLE IF NOT EXISTS tb_item_pedido;
+-- CREATE TABLE tb_item_pedido(
+--   --surrogate key
+--   cod_item_pedido SERIAL PRIMARY KEY,
+--   cod_item INT,
+--   cod_pedido INT,
+--   CONSTRAINT fk_item FOREIGN KEY (cod_item)
+--     REFERENCES tb_item (cod_item),
+--   CONSTRAINT fk_pedido FOREIGN KEY (cod_pedido)
+--     REFERENCES tb_pedido (cod_pedido)
+-- );
+
+-- DROP TABLE IF EXISTS tb_item;
+-- CREATE TABLE tb_item(
+--   cod_item SERIAL PRIMARY KEY,
+--   descricao VARCHAR(200) NOT NULL,
+--   valor NUMERIC(10, 2) NOT NULL,
+--   cod_tipo INT NOT NULL,
+--   CONSTRAINT fk_tipo_item FOREIGN KEY (cod_tipo) REFERENCES tb_tipo_item(cod_tipo)
+-- );
+
+-- INSERT INTO tb_item (descricao, valor, cod_tipo) VALUES
+-- ('Refrigerante', 7, 1),
+-- ('Suco', 8, 1),
+-- ('Hamburguer', 12, 2),
+-- ('Batata frita', 9, 2);
+
+
+
+-- DROP TABLE IF EXISTS tb_tipo_item;
+-- CREATE TABLE tb_tipo_item(
+--   cod_tipo SERIAL PRIMARY KEY,
+--   descricao VARCHAR(200) NOT NULL
+-- );
+-- INSERT INTO tb_tipo_item (descricao) VALUES
+-- ('Bebida'), ('Comida');
+
+-- SELECT * FROM tb_tipo_item;
+
+
+-- DROP TABLE IF EXISTS tb_cliente;
+-- CREATE TABLE tb_cliente(
+--   cod_cliente SERIAL PRIMARY KEY,
+--   nome VARCHAR(200) NOT NULL
+-- );
+
+-- DROP TABLE IF EXISTS tb_pedido;
+-- CREATE TABLE IF NOT EXISTS tb_pedido(
+--   cod_pedido SERIAL PRIMARY KEY,
+--   data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--   data_modificacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--   status VARCHAR DEFAULT 'aberto',
+--   cod_cliente INT NOT NULL,
+--   CONSTRAINT fk_cliente FOREIGN KEY (cod_cliente) REFERENCES tb_cliente(cod_cliente)
+-- );
 
 
 
